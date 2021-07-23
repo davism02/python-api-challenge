464f5ddc38d2525aa438bc73ddb21c8bbe694e45
Michelle Davis <Michelle.Davis1@ipaper.com>
2021-07-23 18:08:39 -0400

    first draft

---
+++ VacationPy/config.py
@@ -0,0 +1,6 @@
+# Add your API key
+api_key = 'f21b6ce7d992394778efb64f0dc64f2d'
+weather_api_key = "f21b6ce7d992394778efb64f0dc64f2d"
+g_key = "AIzaSyAVWaSYFICoqyg3v9IeRSVFzjSpmQxeNz"
+googlemaps = "AIzaSyChdaH0egBIM9t1NfqlVhX_Pl_vGtX2TLw"
+census_api ="a63554692ef6a9a266b91f08e474b49b100caeef"
